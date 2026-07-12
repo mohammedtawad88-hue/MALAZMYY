1a466e106ec0f5ef531ada2fe100df0c8c0bc370
mohammedtawad88-hue <mohammedtawad88@gmail.com>
2026-07-12 13:11:24 +0300

    Fix: Add @OptIn(ExperimentalForeignApi::class) annotation to PlatformPdfWebView for iOS

--- composeApp/src/iosMain/kotlin/com/example/ui/PlatformPdfWebView.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/ui/PlatformPdfWebView.ios.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun PlatformPdfWebView(url: String, modifier: Modifier) {
     UIKitView(
